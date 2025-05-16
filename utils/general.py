dcbfba2ff18e7eb724f8112aaee772021ef8d9bd
hamanakakohei <hamanaka193@gmail.com>
2025-05-16 19:47:08 +0900

    ccc

--- utils/general.py
+++ utils/general.py
@@ -2,6 +2,7 @@
 
 
 import requests
+import pandas as pd
 
 
 def download_from_URLs( urls ):
@@ -12,4 +13,24 @@ def download_from_URLs( urls ):
     response.raise_for_status()   # エラーがあれば例外を投げる
     with open(filename, "wb") as f:
       f.write(response.content)
+
+
+def extract_filenames_from_urls( urls ):
+  filenames = []
+  for url in urls:
+    filename = url.strip().split('/')[-1]
+    filenames.append( filename )
+  
+  return filenames
+
+
+def make_concatenated_df( files, **read_kwargs ):
+  dfs = []
+  for file in files:
+    df = pd.read_csv( file, **read_kwargs )
+    df['__source__'] = file  # 元ファイル名を記録
+    dfs.append( df )
+  
+  concatenated_df = pd.concat( dfs, ignore_index=True )
+  return concatenated_df
 
